9a061bd4f6ec51cd5ad6569dea06784ff4cdfd23
Rasmus Haavisto <rasmus.haavisto@gmail.com>
2016-07-30 13:44:04 +0300

    Fixes

--- Rapidnaming/src/main/java/fi/utu/ville/exercises/rapidnaming/RapidnamingEditor.java
+++ Rapidnaming/src/main/java/fi/utu/ville/exercises/rapidnaming/RapidnamingEditor.java
@@ -55,6 +55,7 @@ public class RapidnamingEditor implements MathTabbedEditorWrap<RapidnamingData> 
 		this.rapidnamingMode = rapidnamingMode;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public VerticalLayout drawSettings() {
 
